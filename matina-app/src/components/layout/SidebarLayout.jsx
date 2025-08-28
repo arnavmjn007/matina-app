07a2ccf2946bd704a3c3e7e20cbe762e690b4faa
arnavmjn007 <arnavmjn007@gmail.com>
2025-08-28 22:57:35 +0545

    Added responsiveness

--- matina-app/src/components/layout/SidebarLayout.jsx
+++ matina-app/src/components/layout/SidebarLayout.jsx
@@ -4,9 +4,7 @@ const SidebarLayout = ({ children, navigateTo, currentPage }) => {
     return (
         <div className="flex h-screen overflow-hidden bg-rose-50">
             {/* Sidebar */}
-
             <Sidebar navigateTo={navigateTo} currentPage={currentPage}  />
-
             {/* Main Content */}
             <main className="flex-grow p-8 sm:p-10 overflow-y-auto">
                 {children}
