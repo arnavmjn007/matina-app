ba7029f0fedc11d05f612ccb97257de9825a5465
arnavmjn007 <arnavmjn007@gmail.com>
2026-01-05 17:45:34 +0545

    fixed conflicts

--- matina-backend/src/main/java/com/matina/matina_app/config/SecurityConfig.java
+++ matina-backend/src/main/java/com/matina/matina_app/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/users/register", "/api/users/login").permitAll()
 
                         // ✅ Ghost mode reads
-                        .requestMatchers(HttpMethod.GET, "/api/users/discovery").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/discovery/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users/profiles/**").permitAll()
 
                         // 2) PROTECTED
